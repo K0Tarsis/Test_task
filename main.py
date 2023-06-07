83f75e1139ed9164fd3369016208915606e6fa7a
Kostiantyn <kostiantyn.hovorukha@gmail.com>
2023-06-07 14:16:54 +0300

    v2

--- main.py
+++ main.py
@@ -2,34 +2,39 @@ import tensorflow as tf
 import os
 import numpy as np
 import click
-from skimage import img_as_ubyte
-from skimage.io import imread, imsave
+from skimage.io import imread
+from PIL import Image
 from skimage.morphology import binary_opening, disk
-from tools import gdice_coef_loss, dice_coef, true_positive_rate, zero_IoU, IoU, agg_loss
 
 
-fullres_model = tf.keras.models.load_model('model', custom_objects={
-            "gdice_coef_loss": gdice_coef_loss,
-            "dice_coef": dice_coef,
-            "true_positive_rate": true_positive_rate
-        })
+fullres_model = tf.keras.models.load_model('model/fullres_model.h5')
 
 
-def predict(img_p):
-    c_img = imread(img_p)
-    c_img = np.expand_dims(c_img, 0)/255.0
-    cur_seg = fullres_model.predict(c_img)[0]
-    cur_seg = binary_opening(cur_seg>1e3, np.expand_dims(disk(2), -1))
-    img_n = img_p.split('/')[-1]
-    return cur_seg, c_img, img_n
+def raw_prediction(path):
+    c_img = imread(path)
+    pre_img = np.expand_dims(c_img, 0)/255.0
+    cur_seg = fullres_model.predict(pre_img)[0]
+    return cur_seg, c_img
+
+
+def smooth(cur_seg):
+    return binary_opening(cur_seg>0.99, np.expand_dims(disk(2), -1))
+
+
+def predict(path='Data/08cd21e12.jpg'):
+    img_n = path.split('/')[-1]
+    cur_seg, c_img = raw_prediction(path=path)
+    return smooth(cur_seg), c_img, img_n
 
 
 def pred_img(i_p, save_f):
-    c_s, c_i, c_n = predict(i_p)
+    seg, img, name = predict(i_p)
+    img = Image.fromarray(img)
+    seg = Image.fromarray(seg[:, :, 0])
     if os.path.isdir(f"{save_f}") is False:
         os.mkdir(f"{save_f}")
-    imsave(f"{save_f}/c_s_{c_n}", img_as_ubyte(c_s))
-    imsave(f"{save_f}/c_i_{c_n}", c_i[0])
+    img.save(f"{save_f}/img_{name}")
+    seg.save(f"{save_f}/seg_{name}")
 
 
 def main_f(f_path, stream, save_f):
@@ -44,7 +49,7 @@ def main_f(f_path, stream, save_f):
 
 @click.command()
 @click.argument('type', default='image')
-@click.argument('path_t', default='Data/733337296.jpg')
+@click.argument('path_t', default='Data/08cd21e12.jpg')
 @click.argument('save_f', default='Done')
 def main(type, path_t, save_f):
     if type == "stream":
